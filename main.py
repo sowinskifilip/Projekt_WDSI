0d902af845275b056b6db238ca6a59a6526b8d81
Filip Sowinski <97602694+sowinskifilip@users.noreply.github.com>
2022-02-09 14:47:48 +0100

    Data read update

--- main.py
+++ main.py
@@ -7,28 +7,40 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas
 from xml.etree import ElementTree
 
-def load_data(path):
-    # path_ann = os.path.join(path, 'annotations')
-    # list_ann = os.listdir(path_ann)
-
-    path_img = os.path.join(path, 'images')
-
+def split_data(path):
     path_ann = os.path.join(path, 'annotations/*.xml')
     list_ann = glob.glob(path_ann)
 
+    path_img = os.path.join(path, 'images/*.png')
+    list_img = glob.glob(path_img)
 
-    names = []
+    data = []
+    data_speed = []
+    data_nonespeed = []
 
-    for el in list_ann:
-        dom = ElementTree.parse(el)
-        names.append(dom.findall('object/name'))
+    if(len(list_img) == len(list_ann)):
+        for el in range(len(list_ann)):
+            data.append({'annotation': list_ann[el], 'image': list_img[el], 'speedlimit': None})
 
-    print(names)
+    for el in data:
+        parser = ElementTree.parse(el['annotation'])
+        for object in parser.findall('object'):
+            name = object.findall('name')
+            for n in name:
+                if(n.text == 'speedlimit'):
+                    el['speedlimit'] = 1
+
+    for el in data:
+        if(el['speedlimit'] == 1):
+            data_speed.append(el)
+        else:
+            data_nonespeed.append(el)
+
     return
 
 
 def main():
-    load_data('data')
+    split_data('data')
     return
 
 if __name__ == '__main__':
